8dd76ce871f0e78972fac26b460a02539e016618
Phanh <anh.pham.210191@student.fulbright.edu.vn>
2025-12-09 00:10:24 +0700

    func: add viewport for home page

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,13 +1,11 @@
 import React from "react";
-import styles from "./home.css";
+import styles from "./home.module.css";
 
 const Home = React.forwardRef((props, ref) => {
   return (
     <section ref={ref} className={styles.section}>
-      <h1 className={styles.title}>Welcome to My Portfolio</h1>
-      <p className={styles.text}>
-        I'm a passionate developer who enjoys building clean, functional, and creative digital experiences.
-      </p>
+      <div className={styles.stars}></div>
+      <h1 className={styles.name}>Aerina</h1>
     </section>
   );
 });
