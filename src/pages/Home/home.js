424e26196410b76ac057d37e4a7487def22a78d6
Phanh <anh.pham.210191@student.fulbright.edu.vn>
2025-12-15 23:05:31 +0700

    Update content

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -5,7 +5,7 @@ const Home = React.forwardRef((props, ref) => {
   return (
     <section ref={ref} className={styles.section}>
       <div className={styles.stars}></div>
-      <h1 className={styles.name}>Aerina</h1>
+      <h1 className={styles.name}>Aerin</h1>
     </section>
   );
 });
